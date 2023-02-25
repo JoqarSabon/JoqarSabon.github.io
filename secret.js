ee4d21e880a8ece72f8764601c5087cd77fe1bc7
Johannes <95719947+JoqarSabon@users.noreply.github.com>
2023-02-25 11:57:19 +0100

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,4 +1,7 @@
-window.addEventListener('contextmenu', (event) => {
-    event.preventDefault();
-    alert('https://joqarsabon.github.io/backrooms.html ???');
-    });
+document.addEventListener('mousedown', e => {
+     if (e.which === 2) {
+          window.alert('??????');
+          window.location.href='https://joqarsabon.com/backrooms.html';
+          e.preventDefault()
+        }
+      });
